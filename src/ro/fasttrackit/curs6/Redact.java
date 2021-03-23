f3559cfb0cdb0e3b3c0fd10df3151436ad53b819
Sipos Dorel Ciprian <ciprian_sipos@yahoo.com>
2021-03-23 10:21:38 +0200

    curs 6 tema

--- src/ro/fasttrackit/curs6/Redact.java
+++ src/ro/fasttrackit/curs6/Redact.java
@@ -1,8 +1,72 @@
 package ro.fasttrackit.curs6;
 
+import java.util.Arrays;
+
 public class Redact {
     public static void main(String[] args){
-        System.out.println("Hello Github");
+       // System.out.println("Hello Github");
+       int count =  countChar("testare",'t');
+        System.out.println(count);
+        String redacted = redact("words");
+        System.out.println(redacted);
+        System.out.println(redactSimpleMethod("words"));
+        System.out.println(contains(new String[]{"Ana","are","mere"},"mere"));
+        System.out.println(Arrays.toString(phraseToArray("Ana are mere multe")));
+        System.out.println(sri("Ana are mere multe si marunte", new String[]{"mere","marunte","Ana"}));
+    }
+    private static String sri(String phrase, String[] censorWords){
+        String[] phraseToWords = phraseToArray(phrase);
+        String censored = "";
+        for(String word : phraseToWords){
+            if(contains(censorWords, word)) {
+                censored = censored + redact(word) + " ";
+            }else{
+                censored = censored + word + " ";
+            }
+        }
+        return censored;
+    }
+
+
+
+    private static String[] phraseToArray(String phrase){
+        return phrase.split(",");
+    }
+
+    public static boolean contains(String[] words,String search){
+        for(String word: words){
+            if(word.equalsIgnoreCase(search)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    public static String redactSimpleMethod(String word){
+        return word.replaceAll(".","*");
+    }
+
+    public static String redact(String word){
+       int length = word.length();
+       String result = "";
+       int i=0;
+       while(i!=length){
+           result = result +"*";
+           i++;
+       }
+       return result;
+
+    }
+
+    public static int countChar(String word, char letterToSearch){
+        int count =0;
+        for(char wordLetter : word.toCharArray()){
+            if(wordLetter == letterToSearch) {
+                count++;
+            }
+        }
+        return count;
     }
 
 }
---
+++ src/ro/fasttrackit/curs6homework/Course6Homework.java
@@ -0,0 +1,76 @@
+package ro.fasttrackit.curs6homework;
+
+import java.util.Scanner;
+import java.lang.Math;
+
+public class Course6Homework {
+    public static void main(String[] args){
+        int[] numbers = new int[]{1, 25, 54, 75, 23, 5, 4, 9};
+        System.out.println("Sum of the elements is: " + sumOfElements(numbers));
+        System.out.println("There are " + oddElements(numbers)+ " odd elements");
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Insert a number: ");
+        int n = input.nextInt();
+        System.out.println("Elements greater than "+ n+" are: ");
+        checkIfGreater(numbers,n);
+        System.out.println("--------");
+
+        //int targetAmount;
+        //System.out.println(randomDonation(100));
+
+        System.out.println("--------");
+        //targetedAmount(100);
+
+        String s= "Scrieti o functie care primeste un numar intreg reprezentand targetul de donatii.Donatiile se vor face cu ajutorul obiectului Random.Primim donatii pana cand ajungem la suma dorita.Cand ajungem la suma dorita afisam un mesaj de succes";
+
+        phrase(s);
+
+    }
+    public static void phrase(String s){
+        String[] arraySplit= s.split("\\.");
+        for (int i =0;i<arraySplit.length;i++){
+            System.out.println(arraySplit[i]);
+        }
+
+    }
+
+
+
+
+    public static int randomDonation(int targetAmount){
+        double d=Math.random()*targetAmount;
+        int donation = (int)d;
+        return donation;
+    }
+
+
+
+
+
+
+
+
+    public static int sumOfElements(int[] numbers){
+        int sum = 0;
+        for(int i :numbers){
+            sum+=i;
+        }
+        return sum;
+    }
+    public static int oddElements(int[] numbers){
+        int count=0;
+        for(int i:numbers){
+            if(i%2 != 0){
+                count++;
+            }
+        }return count;
+    }
+    public static void checkIfGreater(int[] numbers, int n){
+        for(int i : numbers){
+            if(i>n){
+                System.out.println(i);
+            }
+        }
+    }
+}
